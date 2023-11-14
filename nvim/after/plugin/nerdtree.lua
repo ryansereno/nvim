cc7f66e3437f02bdfcffc36e05a832e6d7a55871
ryansereno <ryansereno@protonmail.com>
2023-11-14 15:10:53 -0500

    nvim tree key maps

--- nvim/after/plugin/nerdtree.lua
+++
@@ -1,4 +0,0 @@
-require("nvim-tree").setup {}
-
-vim.api.nvim_set_keymap('n', '<leader>t', ':w<CR>:NvimTreeOpen<CR>', { noremap = true, silent = true })
-
---
+++ nvim/after/plugin/nvim-tree.lua
@@ -0,0 +1,24 @@
+local function my_on_attach(bufnr)
+  local api = require "nvim-tree.api"
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- Custom mapping for 's' to open the file in a vertical split
+    vim.keymap.set('n', 's', api.node.open.vertical, opts('Split'))
+
+end
+
+-- pass to setup along with your other options
+require("nvim-tree").setup {
+  ---
+  on_attach = my_on_attach,
+  ---
+}
+
+-- Global mapping to open nvim-tree with '\t'
+vim.api.nvim_set_keymap('n', '<leader>t', ':w<CR>:NvimTreeOpen<CR>', { noremap = true, silent = true })
