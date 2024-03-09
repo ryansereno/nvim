6d2993f4ef82b36084e3aeadae4a4407c567f42b
ryansereno <ryansereno@protonmail.com>
2024-03-09 07:07:30 -0500

    changed color and icon for untracked git files, nvim-tree

--- nvim/after/plugin/nvim-tree.lua
+++ nvim/after/plugin/nvim-tree.lua
@@ -1,3 +1,5 @@
+--file tree plugin
+
 local function my_on_attach(bufnr)
   local api = require "nvim-tree.api"
 
@@ -9,12 +11,24 @@ local function my_on_attach(bufnr)
   api.config.mappings.default_on_attach(bufnr)
 
   -- Custom mapping for 's' to open the file in a vertical split
-    vim.keymap.set('n', 's', api.node.open.vertical, opts('Split'))
+  vim.keymap.set('n', 's', api.node.open.vertical, opts('Split'))
+
+  -- Custom color for untracked files
+  vim.api.nvim_set_hl(0, 'NvimTreeGitNew', { fg = '#ffb380' })
 
 end
 
 -- pass to setup along with your other options
 require("nvim-tree").setup {
+	  renderer = {
+    icons = {
+      glyphs = {
+        git = {
+          untracked = "*"
+        },
+      }
+    }
+  },
   ---
   on_attach = my_on_attach,
   ---
